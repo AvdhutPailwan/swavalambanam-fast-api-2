8c013dfc623ee3071fc4c83630eaf6a2bd20b5aa
Avdhut Pailwan <avdhutpailwan@gmail.com>
2023-12-15 17:09:57 +0530

    UPDATE: routes/question_and_answer_chatbot.py

--- src/routes/question_and_answer_chatbot.py
+++ src/routes/question_and_answer_chatbot.py
@@ -1,28 +1,23 @@
 from _testcapi import INT_MAX
 
-from fastapi import APIRouter, Request, Form, Body
+from fastapi import APIRouter, Body
 import os
-
-# Imports
+from models.qna import QnA
 from tflite_support.task import text
 
+# path for the loading of the model
 parent_dir_path = str(os.path.dirname(os.path.realpath(__file__)))
-print(parent_dir_path.replace("/src/routes", ""))
 parent_dir_path = parent_dir_path.replace("/src/routes", "")
-# Initialization
+
+# Initialization of the ml model
 answerer = text.BertQuestionAnswerer.create_from_file(parent_dir_path + '/public/ml_models/sample.tflite')
-
-# Run inference
-# bert_qa_result = answerer.answer(context, question)
-
 
 question_and_answer_chatbot_router = APIRouter()
 
 
 @question_and_answer_chatbot_router.post("/", tags=["Question and Answer Chatbot"])
-async def answer(request: Request):
-    input_values = dict(await request.json())
-    bert_qa_result = answerer.answer(input_values['context'], input_values['question'])
+async def answer(input_values: QnA = Body(...)):
+    bert_qa_result = answerer.answer(input_values.context, input_values.question)
     minimum = INT_MAX
     ans = {}
     for result in bert_qa_result.answers:
