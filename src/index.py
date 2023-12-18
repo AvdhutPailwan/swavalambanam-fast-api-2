41de2e062d17061a7809328e7f650fe2636de784
Avdhut Pailwan <avdhutpailwan@gmail.com>
2023-12-18 11:13:41 +0530

    ADD: know your rights

--- src/index.py
+++ src/index.py
@@ -11,6 +11,7 @@ from routes.interest_based_learning import interest_based_learning_router
 from routes.note import note
 from routes.question_and_answer_chatbot import question_and_answer_chatbot_router
 from routes.quiz import quiz
+from routes.know_your_rights import know_your_right_router
 
 load_dotenv()
 
@@ -20,6 +21,7 @@ app.include_router(note, prefix="/notes")
 app.include_router(interest_based_learning_router, prefix="/interest_based_learning")
 app.include_router(question_and_answer_chatbot_router, prefix="/question_and_answer_chatbot")
 app.include_router(quiz, prefix="/quiz")
+app.include_router(know_your_right_router, prefix="/know_your_right")
 
 # @app.post("/upload/audio/")
 # async def upload_audio_file(audio_file: UploadFile = File(...)):
---
+++ src/routes/know_your_rights.py
@@ -0,0 +1,22 @@
+import joblib
+from fastapi import APIRouter, Body
+
+from config.db import db
+from utils.absolute_root import root_path
+
+know_your_right_router = APIRouter()
+
+# load know_your_right_model
+model = joblib.load(root_path + '/public/ml_models/model.pkl')
+vectorizer = joblib.load(root_path + '/public/ml_models/vectorizer.pkl')
+
+
+@know_your_right_router.post("/", tags=["Know Your Rights"])
+def know_your_rights(context: str = Body(...)):
+    act = model.predict(vectorizer.transform([context]))
+    act_name = act.tolist()[0]
+    act_obj = db.rights.find_one({"ACT": act_name})
+    return {
+        "act": act_obj["ACT"],
+        "description": act_obj["Description"]
+    }
