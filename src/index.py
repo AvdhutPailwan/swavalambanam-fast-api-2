f350805ad7699f88c89e9a284c38c9d88c8e8039
Avdhut Pailwan <avdhutpailwan@gmail.com>
2023-12-19 21:35:25 +0530

    UPDATE: rights feature added

--- src/index.py
+++ src/index.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from routes.interest_based_learning import interest_based_learning_router
-# from routes.know_your_rights import know_your_right_router
+from routes.know_your_rights import know_your_right_router
 # , UploadFile, File
 # from fastapi.responses import JSONResponse
 # import shutil
@@ -20,7 +20,7 @@ app = FastAPI()
 app.include_router(interest_based_learning_router, prefix="/interest_based_learning")
 app.include_router(question_and_answer_chatbot_router, prefix="/question_and_answer_chatbot")
 app.include_router(quiz, prefix="/quiz")
-# app.include_router(know_your_right_router, prefix="/know_your_right")
+app.include_router(know_your_right_router, prefix="/know_your_right")
 
 # @app.post("/upload/audio/")
 # async def upload_audio_file(audio_file: UploadFile = File(...)):
